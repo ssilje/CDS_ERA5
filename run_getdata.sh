2ccb1c97c3392c8821a5a280138e44826cb3e0cd
Silje Sørland <silje.soerland@env.ethz.ch>
2020-12-10 14:56:02 +0100

    Update run_getdata.sh

--- run_getdata.sh
+++ run_getdata.sh
@@ -7,7 +7,7 @@ month=$(echo ${datein} | cut -d'-' -f2)
 echo ${datein}
 
 run_dir='/cluster/home/sso102/ERA5/CDS_ERA5'
-savedir='/cluster/work/users/sso102/ERA5'
+savedata='/cluster/work/users/sso102/ERA5/ERA5'
 
 
 if [ ! -d ${run_dir}/jobs ]
@@ -18,16 +18,10 @@ else
     mkdir ${run_dir}/jobs
 fi
 
-if [ ! -d ${savedir} ]
-then
-    mkdir ${savedir}
-
-fi
-
    
 cp $run_dir/getdata_CDS_ERA5.py $run_dir/jobs/getdata_CDS_ERA5${datein}.py 
 sed -i "s/2019/$year/g" $run_dir/jobs/getdata_CDS_ERA5${datein}.py 
 sed -i "s/'month': '01'/'month': ' $month '/g" $run_dir/jobs/getdata_CDS_ERA5${datein}.py
-sed -i "s/DATA/$savedir/ERA5_$datein/g" $run_dir/jobs/getdata_CDS_ERA5${datein}.py
+sed -i "s/DATA/$savedata_$datein/g" $run_dir/jobs/getdata_CDS_ERA5${datein}.py
 
   
