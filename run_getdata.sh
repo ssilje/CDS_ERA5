7ae0455729386899ce71ab32c0182773077ca53f
Silje Sørland <silje.soerland@env.ethz.ch>
2020-12-10 15:00:10 +0100

    Update run_getdata.sh

--- run_getdata.sh
+++ run_getdata.sh
@@ -6,8 +6,8 @@ year=$(echo ${datein} | cut -d'-' -f1)
 month=$(echo ${datein} | cut -d'-' -f2)
 echo ${datein}
 
-run_dir='/cluster/home/sso102/ERA5/CDS_ERA5'
-savedata='/cluster/work/users/sso102/ERA5/ERA5'
+#run_dir='/cluster/home/sso102/ERA5/CDS_ERA5'
+run_dir='/cluster/work/users/sso102/ERA5/'
 
 
 if [ ! -d ${run_dir}/jobs ]
