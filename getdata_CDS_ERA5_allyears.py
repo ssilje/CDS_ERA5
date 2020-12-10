cc65b032de13337b89c0e99a5e2e05dda8b018bd
Silje Sørland <silje.soerland@env.ethz.ch>
2020-12-10 17:15:21 +0100

    Create getdata_CDS_ERA5_allyears.py

---
+++ getdata_CDS_ERA5_allyears.py
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+
+syear=1999
+eyear=2019
+month='01 02 03 04 05 06 07 08 09 10 11 12'
+script_dir='/cluster/home/sso102/ERA5/CDS_ERA5'
+run_dir='/cluster/work/users/sso102/ERA5/'
+
+
+if [ ! -d ${run_dir}/jobs ]
+then
+    mkdir ${run_dir}/jobs
+else
+    rm -r ${run_dir}/jobs
+    mkdir ${run_dir}/jobs
+fi
+
+
+YYYY=${syear}
+
+while [ ${YYYY} -le ${eyear} ]
+do
+
+for m in ${month}; do 
+
+datein=$YYYY$m
+
+cp $script_dir/getdata_CDS_ERA5.py $run_dir/jobs/getdata_CDS_ERA5_${datein}.py 
+sed -i "s/2019/$year/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py 
+sed -i "s/'month': '01'/'month': ' $month '/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
+sed -i "s/DATA/ERA5_$datein/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
+
+if [ ! -f ${run_dir}/jobs/ERA5_${datein}.grb ]
+then
+ echo "running python $run_dir/jobs/getdata_CDS_ERA5_${datein}.py"
+  
+ python  $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
+ wait
+ echo "done..."
+ else 
+ echo " File already downloded "
+fi  
+
+YYYY=`expr ${YYYY} + 1`
+done
+done
+
+
+
+
+
+
