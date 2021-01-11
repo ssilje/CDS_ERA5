9c3d73e14134bd8514c56077955ed266fce8f0b5
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-11 09:18:51 +0100

    Update getdata_CDS_ERA5_allyears.sh

--- getdata_CDS_ERA5_allyears.sh
+++ getdata_CDS_ERA5_allyears.sh
@@ -7,6 +7,7 @@ eyear=2019
 month='01 02 03 04 05 06 07 08 09 10 11 12'
 script_dir='/cluster/home/sso102/ERA5/CDS_ERA5'
 run_dir='/cluster/work/users/sso102/ERA5/'
+savedir='/cluster/work/users/sso102/ERA5/sfc/tp/'
 
 
 if [ ! -d ${run_dir}/jobs ]
@@ -30,7 +31,7 @@ datein=$YYYY$m
 cp $script_dir/getdata_CDS_ERA5.py $run_dir/jobs/getdata_CDS_ERA5_${datein}.py 
 sed -i "s/2019/${YYYY}/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py 
 sed -i "s/'month': '01'/'month': ' $m '/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
-sed -i "s/DATA/ERA5_$datein/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
+sed -i "s/DATA/${savedir}ERA5_${datein}/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
 
 if [ ! -f ${run_dir}/jobs/ERA5_${datein}.grb ]
 then
