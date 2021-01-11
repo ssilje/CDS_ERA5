813edb1ef281c5048671e6b13438ae9c6c4c6cea
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-11 09:56:30 +0100

    Update getdata_CDS_ERA5_allyears.sh

--- getdata_CDS_ERA5_allyears.sh
+++ getdata_CDS_ERA5_allyears.sh
@@ -30,7 +30,7 @@ for m in ${month}; do
 datein=$YYYY$m
 echo ${savedir}ERA5_${datein}
 p1=${savedir}ERA5_${datein}
-$p1=${p1//\//\\/}
+#$p1=${p1//\//\\/}
 #$p2=${p2//\//\\/}
 #sed s/$p1/$p2/ file
 #sed s/${p1//\//\\/}/${p2//\//\\/}/ file
@@ -39,7 +39,7 @@ cp $script_dir/getdata_CDS_ERA5.py $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
 sed -i "s/2019/${YYYY}/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py 
 sed -i "s/'month': '01'/'month': ' $m '/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
 #sed -i "s/DATA/${${savedir}ERA5_${datein}//\//\\/}/g" $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
-sed -i 's|DATA|${savedir}ERA5_${datein}|g' $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
+sed -i 's|DATA|"${savedir}ERA5_${datein}"|g' $run_dir/jobs/getdata_CDS_ERA5_${datein}.py
 
 if [ ! -f ${run_dir}/jobs/ERA5_${datein}.grb ]
 then
